136fcd7d28708dfb53a01dc8aaee4359c0b82fdf
neogamit <77265987+AmitMishra-852@users.noreply.github.com>
2021-01-21 08:02:29 +0530

    final commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var textInputArea=document.querySelector("#textarea")
 var ouputArea=document.querySelector("#divbox")
 
 
-var serverURL="https://api.funtranslations.com/translate/ferb-latin.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
 
 function getserverURL(text) 
 {
@@ -13,10 +13,13 @@ function getserverURL(text)
 
 function myfunction()
 {
+    // read the input text
     var transText=textInputArea.value;
-    
+    // go and fetch the URL
     fetch(getserverURL(transText))
+    // convert url into javascript object
     .then(response=>response.json())
+    //output 
     .then(achualdata=>{
         var translatedText=achualdata.contents.translated;
         ouputArea.innerText=translatedText;
